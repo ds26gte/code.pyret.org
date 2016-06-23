1da222ed39c866a340b8e4ba8067401e8a781344
Dorai Sitaram <ds26gte@yahoo.com>
2016-06-23 09:17:05 -0400

    ...

--- src/web/js/authenticate-storage.js
+++ src/web/js/authenticate-storage.js
@@ -2,15 +2,24 @@
 var storageAPIDeferred = Q.defer();
 var storageAPI = storageAPIDeferred.promise;
 function handleClientLoad(clientId, apiKey) {
+  //console.log('config.google.clientId = ' + config.google.clientId);
+  //clientId = config.google.clientId;
+  //try to get clientId from config.google
+  console.log('doing handleClientLoad ' + clientId + ', ' + apiKey);
   var api = createProgramCollectionAPI(clientId, apiKey, "code.pyret.org", true);
-  
+  console.log('auth/api = ' + JSON.stringify(api));
+
+  console.log('typeof api = ' + (typeof api));
+
   api.then(function(api) {
+    console.log('api successfully created = ' + JSON.stringify(api));
     storageAPIDeferred.resolve(api);
   });
   api.fail(function(err) {
     storageAPIDeferred.reject(err);
     console.log("Not logged in; proceeding without login info", err);
   });
+  /*
   define("gdrive-credentials", [], function() {
     var thisApiKey = apiKey;
     return {
@@ -24,5 +33,5 @@ function handleClientLoad(clientId, apiKey) {
       }
     };
   });
+  */
 }
-
