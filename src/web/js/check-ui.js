a467bbb952d2211f1705197baa7d261127e8d2d1
Dorai Sitaram <ds26gte@yahoo.com>
2017-02-23 13:47:05 -0500

    ...

--- src/web/js/check-ui.js
+++ src/web/js/check-ui.js
@@ -29,13 +29,14 @@
       var ffi = runtime.ffi;
       var cases = ffi.cases;
       var get = runtime.getField;
-      
-      var noFramesMaybeStackLoc = 
+
+      var noFramesMaybeStackLoc =
         runtime.makeFunction(function(n, userFramesOnly) {
           return runtime.ffi.makeNone();
         });
-      
+
       function makeNameHandle(text, loc, color) {
+        console.log('doing makeNameHandle', JSON.stringify(loc.dict['start-line']));
         var anchor = document.createElement("a");
         anchor.classList.add("hinted-highlight");
         anchor.textContent = text;
@@ -63,13 +64,13 @@
 
       function makeGutterMarker(spanHandle, clickFunc) {
         var editor = spanHandle.doc.getEditor();
-        
-        var lineHandle = 
+
+        var lineHandle =
           editor.addLineClass(
             spanHandle.from.line,
             "gutter",
             "failed-test-marker");
-            
+
         function onClick(cm, line, gutter) {
           if (cm.getLineNumber(lineHandle) !== line)
             return;
@@ -94,7 +95,7 @@
             lineHandle.on("delete", onDelete);
           }
         }
-        
+
         function onDelete(line) {
           var spanLineNo = spanHandle.from;
           if (spanLineNo === undefined)
@@ -105,7 +106,7 @@
             lineHandle.on("delete", onDelete);
           }
         }
-        
+
         lineHandle.on("change", onChange);
         lineHandle.on("delete", onDelete);
 
@@ -134,7 +135,7 @@
           });
 
       }
-      
+
       function makeTestHeader(testNumber, loc, isPassing) {
         var header = document.createElement("header");
         var nameHandle   = makeNameHandle("Test " + testNumber, loc,
@@ -146,9 +147,9 @@
         header.appendChild(status);
         return {header : header, handle : handle};
       }
-      
+
       var lastHighlighted = undefined;
-      
+
       var FailingTestSkeleton = function () {
         function FailingTestSkeleton(test, testNumber) {
           var container = document.createElement("div");
@@ -172,7 +173,7 @@
               });
             }
           }
-          
+
           if(runtime.hasField(test, "actual-exn")) {
             this.maybeStackLoc = outputUI.makeMaybeStackLoc(
               runtime, documents, srcloc, get(test, "actual-exn").val.pyretStack);
@@ -204,7 +205,6 @@
         };
 
         /* Replace the placeholder for the failing test with the error rendering */
-
 
         FailingTestSkeleton.prototype.vivify = function vivify(rendering) {
           this.tombstone.appendChild(rendering[0]);
@@ -310,8 +310,8 @@
 
           var tombstone = undefined;
           if (error !== undefined) {
-            tombstone = document.createElement("div"); 
-            tombstone.classList.add("check-block-error"); 
+            tombstone = document.createElement("div");
+            tombstone.classList.add("check-block-error");
             tombstone.addEventListener("click", function (e) {
               _this.highlight();
             });
@@ -334,7 +334,7 @@
         }
 
         CheckBlockSkeleton.prototype.highlight = function highlight() {
-          if (this.tombstone === undefined) 
+          if (this.tombstone === undefined)
             return;
           outputUI.clearEffects();
           lastHighlighted = this;
@@ -353,7 +353,7 @@
         };
 
         CheckBlockSkeleton.prototype.showTest = function showTest(test) {
-          if (expandedCheckBlock !== undefined) 
+          if (expandedCheckBlock !== undefined)
             expandedCheckBlock.hideTests();
           expandedCheckBlock = this;
           this.container.classList.add("expanded");
@@ -363,7 +363,7 @@
         };
 
         CheckBlockSkeleton.prototype.showTests = function showTests() {
-          if (expandedCheckBlock !== undefined) 
+          if (expandedCheckBlock !== undefined)
             expandedCheckBlock.hideTests();
           expandedCheckBlock = this;
           this.container.classList.add("expanded");
@@ -378,7 +378,7 @@
           outputUI.clearEffects();
           lastHighlighted = undefined;
         };
-        
+
         /* Replace the placeholder for the error with the error rendering */
         CheckBlockSkeleton.prototype.vivify = function vivify(rendering) {
           if (this.tombstone === undefined) return;
@@ -395,26 +395,26 @@
 
         return CheckBlockSkeleton;
       }();
-    
+
       var checkBlocks = ffi.toArray(checkResults);
 
       if (checkBlocks.length === 0)
         return;
-        
+
       var checkErroredSkeletons = new Array();
       var testsFailedSkeletons  = new Array();
       var testsPassedSkeletons  = new Array();
-      
+
       var checkResultsContainer = document.createElement("div");
       checkResultsContainer.classList.add("test-results");
       try{
       for(var i = checkBlocks.length - 1; i >= 0; i--) {
         var checkBlock = checkBlocks[i];
         var maybeError  = get(checkBlock, "maybe-err");
-        
+
         var testsPassing  = 0;
         var testsExecuted = 0;
-        
+
         var tests = ffi.toArray(get(checkBlock, "test-results")).
           reverse().
           map(function(test) {
@@ -431,26 +431,25 @@
             }
             return skeleton;
           });
-          
+
         var endedInError    = get(option, "is-some").app(maybeError);
         var allTestsPassing = testsPassing === testsExecuted;
-        
+
         var error = endedInError ? get(maybeError, "value").val : undefined;
-        
+
         var skeleton =
           new CheckBlockSkeleton(
-            get(checkBlock, "name"), 
+            get(checkBlock, "name"),
             get(checkBlock, "loc"),
             { skeletons: tests,
               passing  : testsPassing,
               executed : testsExecuted }, error);
-        
+
         if (endedInError)
           checkErroredSkeletons.push(skeleton);
         checkResultsContainer.appendChild(skeleton.container);
       }
-      
-      
+
       var checkPassedAll      = testsPassedSkeletons.length;
       var checkBlocksErrored  = checkErroredSkeletons.length;
       var checkTotalAll       = checkPassedAll + testsFailedSkeletons.length;
@@ -487,11 +486,10 @@
         }
       }
 
-
       container.append($(checkResultsContainer).prepend(summary));
-      
+
       }catch(e){console.error(e);}
-      
+
       // must be called on the pyret stack
       function vivifySkeleton(skeleton) {
         var error_to_html = errorUI.error_to_html
